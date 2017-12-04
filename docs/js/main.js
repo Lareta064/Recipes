3605406a639f2c275b3ccf95159edee0a0fee252
Lareta <lareta1@yandex.ru>
2017-12-04 18:37:11 +0300

    recipes-3

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-	$(".toggle-link").click(function(){
-		$(".login-page").toggleClass("login-page--block"); return false;
+	//$(".toggle-link").click(function(){
+		//$(".login-page").toggleClass("login-page--block"); return false;
+	//});
+
+	// Вызов слайдера owl-carousel
+	$("#owl-carousel").owlCarousel({
+		singleItem: true,
+		navigation: true,
+		//theme: "reviews-slider-theme",
+		navigationText : ["",""],
+		slideSpeed: 600
 	});
 });
